dca4a6509a8bb295ddd1aedeba872ac8b60bd45a
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2021-04-15 19:43:27 +0000

    add initial setup

---
+++ src/main/java/org/toocoldtocode/crowdfund/CrowdfundApplication.java
@@ -0,0 +1,13 @@
+package org.toocoldtocode.crowdfund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdfundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdfundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gabrielprrd/todone/TodonebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gabrielprrd.todone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodonebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
