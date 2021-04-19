58950b248dcc3cec837ba3c26278bb0070f3b2a1
gabrielprrd <gabriel.afonso.contato@hotmail.com>
2021-04-19 12:46:32 +0100

    add and hide publishable apikey anyway

--- src/main/java/org/toocoldtocode/crowdfund/CrowdfundApplication.java
+++ src/main/java/org/toocoldtocode/crowdfund/CrowdfundApplication.java
@@ -1,5 +1,6 @@
 package org.toocoldtocode.crowdfund;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
